2d878df1c2710dbab50a08fb3042c1ca3e55877a
michaelbotes <michael@ourhood.co.za>
2016-11-30 17:52:37 +0200

    -set custom widget

--- src/App.js
+++ src/App.js
@@ -4,33 +4,24 @@ class App extends React.Component {
     constructor(){
         super();
         this.state = {
-            txt: 'this is the state txt',
-            cat: 0
+            txt: 'this is the state txt'
         }
     }
     update( e ) {
         this.setState({txt: e.target.value})
     }
     render(){
-        let txt = this.props.txt;
-        let cat = this.props.cat;
         return (
             <div>
-                <input type="text" onChange={this.update.bind(this)}/>
-                <h2>{this.state.txt} - {this.state.cat}</h2>
+                <h1>{this.state.txt}</h1>
+                <Widget update={this.update.bind(this)}/>
+                <Widget update={this.update.bind(this)}/>
+                <Widget update={this.update.bind(this)}/>
             </div>
             )
     }
 }
 
-App.propTypes = {
-    txt: React.PropTypes.string,
-    cat: React.PropTypes.number.isRequired
-};
-
-App.defaultProps = {
-    txt: "This is the default text",
-    cat: 2
-};
+const Widget = (props) => <input type="text" onChange={props.update} />;
 
 export default App;
